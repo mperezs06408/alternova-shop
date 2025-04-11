1cd4e17e22665b2ae7014317b272be69ab5cddb8
Santiago Perez <santiago.perez@pragma.com.co>
2025-04-10 20:22:29 -0500

    feat: remove logs

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import { useEffect } from 'react';
-import { HashRouter } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { getProductsList } from './store/thunks/productsThunk';
-import Router from './router/Router';
+import { useEffect } from "react";
+import { HashRouter } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { getProductsList } from "./store/thunks/productsThunk";
+import Router from "./router/Router";
 
 function App() {
   const dispatch = useDispatch();
 
-  useEffect(()=>{
-    console.log('restart')
-    dispatch( getProductsList() );
-  },[])
+  useEffect(() => {
+    dispatch(getProductsList());
+  }, []);
 
   return (
     <HashRouter>
       <Router />
     </HashRouter>
-  )
+  );
 }
 
-export default App
+export default App;
