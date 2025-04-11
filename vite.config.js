77eaaaf6686d10fc5d6826bb22bc080dd200aad1
Santiago Perez <santiago.perez@pragma.com.co>
2025-04-10 20:14:16 -0500

    chore: vite config changes
    chore: change alias
    feat: remove console log

--- vite.config.js
+++ vite.config.js
@@ -1,46 +1,27 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-// import fs from 'vite-plugin-fs'
-import svgr from 'vite-plugin-svgr'
-import path from 'path'
-import process from 'process'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import path from "path";
+import process from "process";
 
-// https://vitejs.dev/config/
-export default defineConfig( ({command, mode }) => {
-  console.log(command, mode);
+export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd());
 
-  console.log(env)
+  console.log(env);
 
-  return(  {
+  return {
     resolve: {
-      alias:{
-        "@": path.resolve(__dirname, '/src'),
-        "@slices": path.resolve(__dirname, '/src/store/slices'),
-        "@assets": path.resolve(__dirname, '/src/assets'),
-        "@atoms": path.resolve(__dirname,'/src/components/atom'),
-        "@molecules": path.resolve(__dirname,'/src/components/molecules'),
-        "@organisms": path.resolve(__dirname,'/src/components/organisms'),
-        "@template": path.resolve(__dirname,'/src/components/template'),
-        "@styles": path.resolve(__dirname, '/src/styles')
-      }
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@slices": path.resolve(__dirname, "./src/store/slices"),
+        "@assets": path.resolve(__dirname, "./src/assets"),
+        "@atoms": path.resolve(__dirname, "./src/components/atom"),
+        "@molecules": path.resolve(__dirname, "./src/components/molecules"),
+        "@organisms": path.resolve(__dirname, "./src/components/organisms"),
+        "@template": path.resolve(__dirname, "./src/components/template"),
+        "@styles": path.resolve(__dirname, "./src/styles"),
+      },
     },
     plugins: [react(), /*fs(),*/ svgr()],
-  })
-}
-//   {
-//   resolve: {
-//     alias:{
-//       "@": path.resolve(__dirname, '/src'),
-//       "@slices": path.resolve(__dirname, '/src/store/slices'),
-//       "@assets": path.resolve(__dirname, '/src/assets'),
-//       "@atoms": path.resolve(__dirname,'/src/components/atom'),
-//       "@molecules": path.resolve(__dirname,'/src/components/molecules'),
-//       "@organisms": path.resolve(__dirname,'/src/components/organisms'),
-//       "@template": path.resolve(__dirname,'/src/components/template'),
-//       "@styles": path.resolve(__dirname, '/src/styles')
-//     }
-//   },
-//   plugins: [react(), fs(), svgr()],
-// }
-)
+  };
+});
